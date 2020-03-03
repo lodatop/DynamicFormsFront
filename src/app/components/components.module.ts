c8fda27862bb7e02272fd5eca53add508a2e16ac
Wisam <wisam_mozalbat@hotmail.com>
2020-03-02 23:07:42 -0400

    component folder created

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+import { IonicModule } from '@ionic/angular';
+
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+  imports: [IonicModule, FormsModule, CommonModule],
+  declarations:
+  [  ],
+  exports:
+  [  ]
+})
+export class ComponentModule {}
