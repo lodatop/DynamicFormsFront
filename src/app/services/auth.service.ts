5f63349887a99114f571035e47f71da2e89cb815
Paolo Lodato <lodatop@gmail.com>
2020-03-07 13:22:47 -0400

    auth service updated

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -26,12 +26,14 @@ export class AuthService {
          return this.http.post<Response>(`${serverUrl}/user/login`, JSON.stringify(body), httpOptions);
   }
 
-  onRegister(name: string, username: string , email: string, password: string) {
+  onRegister(name: string, username: string , email: string, password: string, age: number, gender: string) {
     const body = {
         password: password,
         username: username,
         name: name,
-        email: email
+        email: email,
+        age: age,
+        gender: gender
     };
     const serverUrl = this.url;
     const httpOptions = {
@@ -42,12 +44,14 @@ export class AuthService {
    return this.http.post<Response>(`${serverUrl}/user/register`, JSON.stringify(body), httpOptions);
   }
 
-  onAdminRegister(name: string, username: string , email: string, password: string) {
+  onAdminRegister(name: string, username: string , email: string, password: string, age: number, gender: string) {
     const body = {
         password: password,
         username: username,
         name: name,
-        email: email
+        email: email,
+        age: age,
+        gender: gender
     };
     const serverUrl = this.url;
     const httpOptions = {
@@ -58,11 +62,13 @@ export class AuthService {
    return this.http.post<Response>(`${serverUrl}/user/registerAdmin`, JSON.stringify(body), httpOptions);
   }
 
-  onUpdate(username: string, password: string, name: string) {
+  onUpdate(username: string, password: string, name: string, age: number, gender: string) {
     const body = {
         password: password,
         username: username,
-        name: name
+        name: name,
+        age: age,
+        gender: gender
     };
     const serverUrl = this.url;
     const httpOptions = {
