38c60291e4d25ce15e3be346104ce67b5fcadb04
Wisam <wisam_mozalbat@hotmail.com>
2020-03-11 14:02:28 -0400

    get form stats

--- src/app/services/chart/chart.service.ts
+++ src/app/services/chart/chart.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-// import 'rxjs/add/operator/map';
-import { map } from 'rxjs/operators';
+import { Response } from '../../models/Response.model';
+import { environment } from '../../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChartService {
-
-  constructor(private _http: HttpClient) {}
-
-  
+  url = environment.url;
+  constructor(private http: HttpClient) {}
 
   getFormStadistics(){
-    return this._http.get("https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22")
-      // map(result => result);
+    const serverUrl = this.url;
+    // return this.http.get<Response>(`${serverUrl}/form`,  {withCredentials: true});
+    return this.http.get("https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22")
   }
 
 }
