e580322f909b102d9d32acb4010a8a29305287d3
Paolo Lodato <lodatop@gmail.com>
2020-03-12 14:50:31 -0400

    deletes

--- src/app/components/form-item/form-item.component.ts
+++ src/app/components/form-item/form-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, DoCheck } from '@angular/core';
 import { OptionService } from '../../services/option/option.service';
 import { MenuService } from '../../services/menu/menu.service';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { NavController } from '@ionic/angular';
 
@@ -10,7 +10,7 @@ import { NavController } from '@ionic/angular';
   templateUrl: './form-item.component.html',
   styleUrls: ['./form-item.component.scss'],
 })
-export class FormItemComponent implements OnInit, DoCheck {
+export class FormItemComponent implements OnInit {
 
   @Input() menuId: string;
   // submenuList: any = [{title_menu: "menu prueba", description_menu: "este es el menu de prueba", id_menu: '1'}];
@@ -26,13 +26,15 @@ export class FormItemComponent implements OnInit, DoCheck {
     private nav: NavController,
     private router: Router) { 
       this.userIsAdm = this.auth.userIsAdmin();
+
+      this.router.events.subscribe((e) => {
+        if (e instanceof NavigationEnd) {
+            this.getData()
+        }
+      });
     }
 
   ngOnInit() {
-    this.getData();
-  }
-
-  ngDoCheck(){
     this.getData();
   }
   
@@ -60,5 +62,17 @@ export class FormItemComponent implements OnInit, DoCheck {
   addFormToSubmenu(){
     this.opt.setOption(2);
     this.nav.navigateForward([`/views/add/${this.menuId}`]);
+  }
+
+  deleteSubmenu(menuId: string){
+    this.menu.deleteMenu(menuId).subscribe(()=>{
+      this.getData()
+    })
+  }
+
+  deleteForm(formId: string){
+    this.opt.deleteForm(formId).subscribe(()=>{
+      this.getData()
+    })
   }
 }
